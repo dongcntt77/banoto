020f7e7805ada96d5d3666294b2702c8904a12bf
dongcntt77 <36391847+dongcntt77@users.noreply.github.com>
2020-08-31 15:28:23 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
+import { ChitietComponent } from './chitiet/chitiet.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+const routes: Routes = [
+  {
+    path: 'chitiet',
+    component: ChitietComponent,
+  },
+  {
+    path: '',
+    component: MainComponent,
+  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChitietComponent,
+    MainComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
