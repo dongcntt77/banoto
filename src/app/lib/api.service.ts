8ba6f91a0cd224772a7b273c52f42a21bbbb417f
dongcntt77 <36391847+dongcntt77@users.noreply.github.com>
2020-09-24 09:36:43 +0700

    update

--- src/app/lib/api.service.ts
+++ src/app/lib/api.service.ts
@@ -1,4 +1,3 @@
-
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
@@ -6,12 +5,12 @@ import { catchError, map } from 'rxjs/operators';
 import { throwError as observableThrowError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-  public host = 'http://localhost:52872'; 
-  constructor(private _http: HttpClient, public router: Router) { }
-  
+  public host = 'http://localhost:52872';
+  constructor(private _http: HttpClient, public router: Router) {}
+
   post(url: string, obj: any) {
     const body = JSON.stringify(obj);
     let cloneHeader: any = {};
@@ -19,29 +18,37 @@ export class ApiService {
     const headerOptions = new HttpHeaders(cloneHeader);
     return this._http
       .post<any>(this.host + url, body, { headers: headerOptions })
-      .pipe(map((res: any) => {
-        let json = res;
-        return json;
-      })).pipe(
+      .pipe(
+        map((res: any) => {
+          let json = res;
+          return json;
+        })
+      )
+      .pipe(
         catchError((err: Response) => {
           return this.handleError(err);
-        }));
+        })
+      );
   }
 
   get(url: string) {
     let cloneHeader: any = {};
     cloneHeader['Content-Type'] = 'application/json';
     const headerOptions = new HttpHeaders(cloneHeader);
-    
+
     return this._http
       .get(this.host + url, { headers: headerOptions })
-      .pipe(map((res: any) => {
-        let json = res;
-        return json;
-      })).pipe(
+      .pipe(
+        map((res: any) => {
+          let json = res;
+          return json;
+        })
+      )
+      .pipe(
         catchError((err: Response) => {
           return this.handleError(err);
-        }));
+        })
+      );
   }
   public handleError(error: any) {
     this.router.navigate(['/err']);
